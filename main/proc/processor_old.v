39fff7fa37b3b092f9e8217d1b11bfa8f76e2cfd
Saagar Arya <saagar.arya@duke.edu>
2025-02-20 22:09:51 -0500

    save. BLT BNE J JAL JR NEG POS

--- main/proc/processor_old.v
+++ main/proc/processor_old.v
@@ -17,7 +17,7 @@
  *
  *
  */
-module processor(
+module processor_old(
     // Control signals
     clock,                          // I: The master clock
     reset,                          // I: A reset signal
@@ -63,39 +63,34 @@ module processor(
 	input [31:0] data_readRegA, data_readRegB;
 
 	/* YOUR CODE STARTS HERE */
-    // ================Program Counter=================== //
-    wire branch, jump, jal, jr;
-    wire[31:0] nextPC, branching_PC, target, PC_sum_out;
+    
+    wire[31:0] nextPC;
+	wire jump, branch;
+    wire[31:0] pc_sum, branch_1, branch_2;
     register_32_bit PC(.q(address_imem), .d(nextPC), .clk(clock), .en(1'b1), .clr(reset));
-    assign branching_PC = branch ? dxpc_out : address_imem;
-    assign target = branch ? {15'd0, dxinsn_out[16:0]} : 32'd0;
-	sum PC_sum(.in1(branching_PC), .in2(target), .cin(~branch), .out(PC_sum_out));
-    assign nextPC = jr ? dxa_out : (jump|jal) ? {5'd0, dxinsn_out[26:0]} : PC_sum_out;
+    // jump set to target
+
+    assign branch_1 = branch ? dxpc_out : address_imem;
+    assign branch_2 = branch ? ({15'd0, dxinsn_out[16:0]}) : 32'd0;
+
+    sum PC_sum(.in1(branch_1), .in2(branch_2), .cin(~(jump|branch)), .out(pc_sum));
+    assign nextPC = isJR ? dxb_out : (jump ? {5'd0, dxinsn_out[26:0]} : pc_sum);
+    
     // ================FETCH STAGE=================== //
 
     // Latch instruction from imem
     wire [31:0] fdinsn_out;
     register_32_bit FD_INSN(.q(fdinsn_out), .d(q_imem), .clk(~clock), .en(1'b1), .clr(reset));
-    
     // Latch PC
     wire [31:0] fdpc_out;
     register_32_bit FD_OP(.q(fdpc_out), .d(address_imem), .clk(~clock), .en(1'b1), .clr(reset));
-    
     // ================DECODE STAGE================== //
-    // Instruction decoder decode stage
+     
     wire [31:0] op_decoder_decode;
     assign op_decoder_decode = 32'b1 << fdinsn_out[31:27];
-    wire r_type_decode, i_type_decode, ji_type_decode, jii_type_decode;
-    
-    // type of instruction
-    // assign r_type_decode = op_decoder_decode[0];
-    assign i_type_decode = op_decoder_decode[5] | op_decoder_decode[7] | op_decoder_decode[8] | op_decoder_decode[2] | op_decoder_decode[6];
-    // assign ji_type_decode = op_decoder_decode[1] | op_decoder_decode[3] | op_decoder_decode[22] | op_decoder_decode[21];
-    assign jii_type_decode = op_decoder_decode[4];
-
     // Decode RS RT register 
-    assign ctrl_readRegA = ((i_type_decode | jii_type_decode) ? fdinsn_out[26:22] : fdinsn_out[21:17]);
-    assign ctrl_readRegB = (i_type_decode ? fdinsn_out[21:17] : fdinsn_out[16:12]);
+    assign ctrl_readRegA = fdinsn_out[21:17];
+    assign ctrl_readRegB = (op_decoder_decode[2]|op_decoder_decode[6]) ? fdinsn_out[26:22] : fdinsn_out[16:12];
 
     // Latch data from RS RT 
     wire [31:0] dxa_out;
@@ -106,43 +101,38 @@ module processor(
     // Latch instruction
     wire [31:0] dxinsn_out;
     register_32_bit DX_INSN(.q(dxinsn_out), .d(fdinsn_out), .clk(~clock), .en(1'b1), .clr(reset));
-    
     // Latch PC
     wire [31:0] dxpc_out;
     register_32_bit DX_OP(.q(dxpc_out), .d(fdpc_out), .clk(~clock), .en(1'b1), .clr(reset));
 
     // ================EXECUTE STAGE================= //
-        
-    wire [31:0] alu_out, op_decoder_execute, data_A, data_B;
-    wire [4:0] alu_op, shiftamt;
+
+    wire [31:0] alu_out, immediate, op_decoder_execute, data_A, data_B;
+    wire [4:0] alu_op;
     wire isNotEqual, isLessThan, overflow;
-    assign shiftamt = dxinsn_out[11:7];
-    
-    // Instruction decoder Execute stage
-    // wire r_type_execute, i_type_execute, ji_type_execute, jii_type_execute;
+
+    // Sign-extend immediate value
+    assign immediate = {{15{dxinsn_out[16]}}, dxinsn_out[16:0]};
+
+    // Decode operation type
     assign op_decoder_execute = 32'b1 << dxinsn_out[31:27];
-    // type of instruction
-    // assign r_type_execute = op_decoder_execute[0];
-    // assign i_type_execute = op_decoder_execute[5] | op_decoder_execute[7] | op_decoder_execute[8] | op_decoder_execute[2] | op_decoder_execute[6];
-    // assign ji_type_execute = op_decoder_execute[1] | op_decoder_execute[3] | op_decoder_execute[22] | op_decoder_execute[21];
-    // assign jii_type_execute = op_decoder_execute[4];
+    wire isImmediate = op_decoder_execute[5];
+    wire isJump = op_decoder_execute[1];
+    wire isBNE = op_decoder_execute[2];
+    wire isJAL = op_decoder_execute[3];
+    wire isJR = op_decoder_execute[4];
+    wire isBLT = op_decoder_execute[6];
+
+    // Select ALU operand and opcode
+    assign data_A = isBLT ? dxb_out : dxa_out;
+    assign data_B = isBLT ? dxa_out : (isImmediate ? immediate : dxb_out);
+    assign alu_op = (isBNE|isBLT) ? 5'd1 : (isImmediate ? 5'b0 : dxinsn_out[6:2]);
     
-    wire addi, bne, blt;
-    assign addi = op_decoder_execute[5]; 
-    assign bne = op_decoder_execute[2];
-    assign blt = op_decoder_execute[6];
-    assign jump = op_decoder_execute[1];
-    assign jal = op_decoder_execute[3];
-    assign jr = op_decoder_execute[4];
-
-    assign data_A = addi ? dxb_out : dxa_out;
-    assign data_B = addi ? {{15{dxinsn_out[16]}}, dxinsn_out[16:0]} : dxb_out;
-    assign alu_op = (bne | blt) ? 5'd1 : (addi ? 5'b0 : dxinsn_out[6:2]);
-    // ALU
-    alu ALU(.data_operandA(data_A), .data_operandB(data_B), .ctrl_ALUopcode(alu_op), .data_result(alu_out), .ctrl_shiftamt(shiftamt), .overflow(overflow), .isNotEqual(isNotEqual), .isLessThan(isLessThan)); 
-
-    //set branch if BNE or BLT conditions are met
-    assign branch = (bne & isNotEqual) | (blt & isLessThan);
+    // Use ALU to compute result
+    alu ALU(.data_operandA(data_A), .data_operandB(data_B), .ctrl_ALUopcode(alu_op), .data_result(alu_out), .ctrl_shiftamt(dxinsn_out[11:7]), .overflow(overflow), .isNotEqual(isNotEqual), .isLessThan(isLessThan)); 
+    
+    assign jump = (isJump|isJAL);
+    assign branch = (isBNE & isNotEqual) | (isBLT & isLessThan);
 
     // Latch ALU result
     wire [31:0] xmo_out;
@@ -151,11 +141,11 @@ module processor(
     // Latch instruction
     wire [31:0] xminsn_out;
     register_32_bit XM_INSN(.q(xminsn_out), .d(dxinsn_out), .clk(~clock), .en(1'b1), .clr(reset));
-    
     // Latch PC
     wire [31:0] xmpc_out;
     register_32_bit XM_OP(.q(xmpc_out), .d(dxpc_out), .clk(~clock), .en(1'b1), .clr(reset));
     
+
     // ================Memory STAGE================= //
     assign wren = 1'd0;
     assign address_dmem = 32'd0;
@@ -174,19 +164,16 @@ module processor(
     
     // ================WRITEBACK STAGE=============== //
 
-    // Instruction decoder writeback stage
     wire [31:0] op_decoder_write;
     assign op_decoder_write = 32'b1 << mwinsn_out[31:27];
     
     // Set destination register and data to write
-    // if JAL[3] set reg r31 to PC+!
     assign ctrl_writeReg = op_decoder_write[3] ? 5'b11111 : mwinsn_out[26:22];
     assign data_writeReg = op_decoder_write[3] ? mwpc_out : mwo_out;
     
-    // Set write enable for ALU Op and addi
+    // Set write enable
     assign ctrl_writeEnable = op_decoder_write[0] | op_decoder_write[5] | op_decoder_write[3];
 	
-
 	/* END CODE */
 
 endmodule
